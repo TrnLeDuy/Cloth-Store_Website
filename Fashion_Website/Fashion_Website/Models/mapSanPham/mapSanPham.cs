02b6189c406ae041a31541a7cabfdb71f6d27ea5
Kha Hoang <hkha928@gmail.com>
2023-04-15 00:10:24 +0700

    Auto stash before checking out "origin/HoangKha_workspace"

---
+++ Fashion_Website/Fashion_Website/Models/mapSanPham/mapSanPham.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fashion_Website.Models.mapSanPham
+{
+    public class mapSanPham
+    {
+        public SanPham ChiTietSanPham(String masp)
+        {
+            fashionDBEntities db = new fashionDBEntities();
+            return db.SanPhams.SingleOrDefault(m => m.MaSP == masp);
+        }
+    }
+}
